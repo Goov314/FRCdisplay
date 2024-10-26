a89e9440c29af6cd3a1fef4a65cfa4a7e4718fd1
Goov314 <84781801+Goov314@users.noreply.github.com>
2024-10-26 23:46:06 +0000

    Sixth commit

---
+++ getdata.py
@@ -0,0 +1,165 @@
+import streamlit as st
+import requests
+import pandas as pd
+import datetime
+
+token = st.secrets['TOKEN']
+
+def getteamdata(team, year):
+    url = f"https://frc-api.firstinspires.org/v3.0/{year}/teams?teamNumber={team}"
+    payload={}
+    headers = {
+      'Authorization': token,
+      'If-Modified-Since': ''
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return(response.json()["teams"][0])
+
+def getcompetitions(team, year):
+    url = f"https://frc-api.firstinspires.org/v3.0/{year}/events?eventCode=&teamNumber={team}&districtCode=&excludeDistrict=&weekNumber&tournamentType"
+    payload={}
+    headers = {
+      'Authorization': token,
+      'If-Modified-Since': ''
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    events = []
+    currentevents = []
+    eventstartdates = []
+    eventenddates = []
+    for event in data["Events"]:
+      if event["allianceCount"] == "EightAlliance":
+        events.append(event["name"])
+        currentevents.append(event["code"])
+        eventstartdates.append(event["dateStart"])
+        eventenddates.append(event["dateEnd"])
+    eventselect = st.selectbox("Select Event:", events)
+    currentevent = currentevents[events.index(eventselect)]
+    eventstartdate = datetime.datetime.fromisoformat(eventstartdates[events.index(eventselect)]).strftime("%B %d %Y")
+    eventenddate = datetime.datetime.fromisoformat(eventenddates[events.index(eventselect)]).strftime("%B %d %Y")
+    return(currentevent, eventstartdate, eventenddate)
+
+def getschedule(team, year, event, level):
+      matchnumber = []
+      starttime = []
+      red1 = []
+      red2 = []
+      red3 = []
+      blue1 = []
+      blue2 = []
+      blue3 = []
+      score = []
+      rp = []
+      teamrp = 0
+      matchdata = [red1, red2, red3, blue1, blue2, blue3]
+      url = f"https://frc-api.firstinspires.org/v3.0/{year}/schedule/{event}?tournamentLevel={level}&teamNumber={team}"
+      payload={}
+      headers = {
+        'Authorization': token,
+        'If-Modified-Since': ''
+      }
+      response = requests.request("GET", url, headers=headers, data=payload)
+      scheduledata = response.json()
+
+      with st.spinner(text="Fetching Data..."):
+        for match in scheduledata["Schedule"]:
+          matchnumber.append(match["description"])
+          matchtime = datetime.datetime.fromisoformat(match["startTime"])
+          starttime.append(matchtime.strftime("%A, %I:%M%p"))
+          currentmatchnumber = match["matchNumber"]
+          url = f"https://frc-api.firstinspires.org/v3.0/{year}/scores/{event}/{level}?matchNumber={currentmatchnumber}"
+          payload={}
+          headers = {
+            'Authorization': token,
+            'If-Modified-Since': ''
+          }
+          response = requests.request("GET", url, headers=headers, data=payload)
+          matchdatajson = response.json()
+          bluealliance = matchdatajson["MatchScores"][0]["alliances"][0]
+          redalliance = matchdatajson["MatchScores"][0]["alliances"][1]
+          score.append("Red: " + str(redalliance["totalPoints"]) + " Blue: " + str(bluealliance["totalPoints"]))
+          rp.append("Red: " + str(redalliance["rp"]) + " Blue: " + str(bluealliance["rp"]))
+
+          for i in range(len(match["teams"])): 
+            matchdata[i].append(str(match["teams"][i]["teamNumber"]))
+            if i <= 2 and str(match["teams"][i]["teamNumber"]) == team:
+              teamrp += redalliance["rp"]
+            elif i >= 2 and str(match["teams"][i]["teamNumber"]) == team:
+              teamrp += bluealliance["rp"]
+      scheduledf = pd.DataFrame({
+        'Match Number': matchnumber,
+        'Start Time': starttime,
+        'Score': score,
+        'Ranking Points': rp,
+        'Red 1': red1,
+        'Red 2': red2,
+        'Red 3': red3,
+        'Blue 1': blue1,
+        'Blue 2': blue2,
+        'Blue 3': blue3
+      })
+      return(scheduledf, teamrp)
+
+def getteamrank(team, year, event, level):
+      url = f"https://frc-api.firstinspires.org/v3.0/{year}/rankings/{event}?teamNumber={team}"
+      payload={}
+      headers = {
+        'Authorization': token,
+        'If-Modified-Since': ''
+      }
+      response = requests.request("GET", url, headers=headers, data=payload)
+      rankdata = response.json()
+      return(rankdata)
+
+def displayteamdata(rankdata, teamrp):
+      st.write("Rank: " + str(rankdata["Rankings"][0]["rank"]))
+      st.write("W/T/L: " + str(rankdata["Rankings"][0]["wins"]) + "/" + str(rankdata["Rankings"][0]["ties"]) + "/" + str(rankdata["Rankings"][0]["losses"]))
+      st.write("Matches Played: " + str(rankdata["Rankings"][0]["matchesPlayed"]))
+      st.write("Qualification Average Points: " + str(rankdata["Rankings"][0]["qualAverage"]))
+      st.write("Total Ranking Points: " + str(teamrp))
+
+def displayschedule(team, eventstartdate, eventenddate, scheduledf):
+      st.subheader("Event Schedule (" + eventstartdate + " - " + eventenddate + ")")
+      def highlight_team(s):
+          color = '#262730' if s == str(team) else ''
+          return f'background-color: {color}'
+      st.dataframe(scheduledf.style.applymap(highlight_team))
+
+def gettopteams(team, year, event, level):
+      teamrank = []
+      teamnumber = []
+      wtl = []
+      matchesplayed = []
+      avgpoints = []
+      url = f"https://frc-api.firstinspires.org/v3.0/{year}/rankings/{event}?top=10"
+      payload={}
+      headers = {
+        'Authorization': token,
+        'If-Modified-Since': ''
+      }
+      response = requests.request("GET", url, headers=headers, data=payload)
+      eventrankingdata = response.json()
+
+      for currentteam in eventrankingdata["Rankings"]:
+        teamrank.append(str(currentteam["rank"]))
+        teamnumber.append(str(currentteam["teamNumber"]))
+        wtl.append(str(currentteam["wins"]) + "/" + str(currentteam["ties"]) + "/" + str(currentteam["losses"]))
+        matchesplayed.append(str(currentteam["matchesPlayed"]))
+        avgpoints.append(str(currentteam["qualAverage"]))
+      rankingdf = pd.DataFrame({
+      'Rank': teamrank,
+      'Team Number': teamnumber,
+      'Wins/Ties/Losses': wtl,
+      'Average Points': avgpoints,
+      'Matches Played': matchesplayed,
+      })
+      return(rankingdf)
+
+def displayrankings(team, rankingdf):
+      def highlight_team(s):
+        color = '#262730' if str(s) == str(team) else ''
+        return f'background-color: {color}'
+      st.subheader("Event Rankings")
+      st.dataframe(rankingdf.style.applymap(highlight_team))
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,170 +2,35 @@ import streamlit as st
 import requests
 import pandas as pd
 import datetime
-
+from getdata import getteamdata, getcompetitions, getschedule, getteamrank, displayteamdata, displayschedule, gettopteams, displayrankings
 
 token = st.secrets['TOKEN']
-
 st.title("FRC Display")
 year = str(datetime.datetime.now().year)
-team = st.text_input("Team Number", placeholder="1234")
 
+team = st.text_input("Team Number", 
+          value=(st.query_params.team if "team" in st.query_params else None), 
+          placeholder="1234")
 
 
 if team:
-  url = f"https://frc-api.firstinspires.org/v3.0/{year}/teams?teamNumber={team}"
-  payload={}
-  headers = {
-    'Authorization': token,
-    'If-Modified-Since': ''
-  }
-  response = requests.request("GET", url, headers=headers, data=payload)
-  teamdata = response.json()["teams"][0]
+  st.query_params.team = team
 
+  teamdata = getteamdata(team, year)
   st.write("Team " + team + ", " + teamdata["nameShort"] + ", from " + teamdata["schoolName"] + " in " + teamdata["city"] + ", " + teamdata["stateProv"] + ", " + teamdata["country"] + ". Rookie Year: " + str(teamdata["rookieYear"]))
-
-  url = f"https://frc-api.firstinspires.org/v3.0/{year}/events?eventCode=&teamNumber={team}&districtCode=&excludeDistrict=&weekNumber&tournamentType"
-  payload={}
-  headers = {
-    'Authorization': token,
-    'If-Modified-Since': ''
-  }
-  response = requests.request("GET", url, headers=headers, data=payload)
-  data = response.json()
-  events = []
-  currentevents = []
-  eventstartdates = []
-  eventenddates = []
-  for event in data["Events"]:
-      if event["allianceCount"] == "EightAlliance":
-          events.append(event["name"])
-          currentevents.append(event["code"])
-          eventstartdates.append(event["dateStart"])
-          eventenddates.append(event["dateEnd"])
-  eventselect = st.selectbox("Select Event:", events)
-  currentevent = currentevents[events.index(eventselect)]
-  eventstartdate = datetime.datetime.fromisoformat(eventstartdates[events.index(eventselect)]).strftime("%B %d %Y")
-  eventenddate = datetime.datetime.fromisoformat(eventenddates[events.index(eventselect)]).strftime("%B %d %Y")
-
+  
+  currentevent, eventstartdate, eventenddate = getcompetitions(team, year)
   level = st.selectbox("Level:", ("Qualification", "Playoff"))
 
   if currentevent and level:
-    url = f"https://frc-api.firstinspires.org/v3.0/{year}/schedule/{currentevent}?tournamentLevel={level}&teamNumber={team}"
-    payload={}
-    headers = {
-      'Authorization': token,
-      'If-Modified-Since': ''
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    scheduledata = response.json()
-
-    matchnumber = []
-    starttime = []
-    red1 = []
-    red2 = []
-    red3 = []
-    blue1 = []
-    blue2 = []
-    blue3 = []
-    score = []
-    rp = []
-    teamrp = 0
-    matchdata = [red1, red2, red3, blue1, blue2, blue3]
-    with st.spinner(text="Fetching Data..."):
-      for match in scheduledata["Schedule"]:
-          matchnumber.append(match["description"])
-          matchtime = datetime.datetime.fromisoformat(match["startTime"])
-          starttime.append(matchtime.strftime("%A, %I:%M%p"))
-          currentmatchnumber = match["matchNumber"]
-          url = f"https://frc-api.firstinspires.org/v3.0/{year}/scores/{currentevent}/{level}?matchNumber={currentmatchnumber}"
-          payload={}
-          headers = {
-           'Authorization': token,
-           'If-Modified-Since': ''
-          }
-          response = requests.request("GET", url, headers=headers, data=payload)
-          matchdatajson = response.json()
-          bluealliance = matchdatajson["MatchScores"][0]["alliances"][0]
-          redalliance = matchdatajson["MatchScores"][0]["alliances"][1]
-          score.append("Red: " + str(redalliance["totalPoints"]) + " Blue: " + str(bluealliance["totalPoints"]))
-          rp.append("Red: " + str(redalliance["rp"]) + " Blue: " + str(bluealliance["rp"]))
-
-          for i in range(len(match["teams"])): 
-              matchdata[i].append(str(match["teams"][i]["teamNumber"]))
-              if i <= 2 and str(match["teams"][i]["teamNumber"]) == team:
-                teamrp += redalliance["rp"]
-              elif i >= 2 and str(match["teams"][i]["teamNumber"]) == team:
-                teamrp += bluealliance["rp"]
-
-    url = f"https://frc-api.firstinspires.org/v3.0/{year}/rankings/{currentevent}?teamNumber={team}"
-    payload={}
-    headers = {
-      'Authorization': token,
-      'If-Modified-Since': ''
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    rankdata = response.json()
-
-    st.write("Rank: " + str(rankdata["Rankings"][0]["rank"]))
-    st.write("W/T/L: " + str(rankdata["Rankings"][0]["wins"]) + "/" + str(rankdata["Rankings"][0]["ties"]) + "/" + str(rankdata["Rankings"][0]["losses"]))
-    st.write("Matches Played: " + str(rankdata["Rankings"][0]["matchesPlayed"]))
-    st.write("Qualification Average Points: " + str(rankdata["Rankings"][0]["qualAverage"]))
-    st.write("Total Ranking Points: " + str(teamrp))
-
-    # st.write("Rank: " + str(rankdata["Rankings"][0]["rank"]) + " --- " + "W/T/L: " + str(rankdata["Rankings"][0]["wins"]) + "/" + str(rankdata["Rankings"][0]["ties"]) + "/" + str(rankdata["Rankings"][0]["losses"]) + " --- " + "Matches Played: " + str(rankdata["Rankings"][0]["matchesPlayed"]) + " --- " + "Qualification Average Points: " + str(rankdata["Rankings"][0]["qualAverage"]))
-
-    scheduledf = pd.DataFrame({
-        'Match Number': matchnumber,
-        'Start Time': starttime,
-        'Score': score,
-        'Ranking Points': rp,
-        'Red 1': red1,
-        'Red 2': red2,
-        'Red 3': red3,
-        'Blue 1': blue1,
-        'Blue 2': blue2,
-        'Blue 3': blue3
-
-    })
-    st.subheader("Event Schedule (" + eventstartdate + " - " + eventenddate + ")")
-
-    def highlight_team(s):
-        color = '#262730' if s == str(team) else ''
-        return f'background-color: {color}'
-
-    st.dataframe(scheduledf.style.applymap(highlight_team))
-
-    url = f"https://frc-api.firstinspires.org/v3.0/{year}/rankings/{currentevent}?top=10"
-    payload={}
-    headers = {
-      'Authorization': token,
-      'If-Modified-Since': ''
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    eventrankingdata = response.json()
-
-    teamrank = []
-    teamnumber = []
-    wtl = []
-    matchesplayed = []
-    avgpoints = []
-    for currentteam in eventrankingdata["Rankings"]:
-      teamrank.append(str(currentteam["rank"]))
-      teamnumber.append(str(currentteam["teamNumber"]))
-      wtl.append(str(currentteam["wins"]) + "/" + str(currentteam["ties"]) + "/" + str(currentteam["losses"]))
-      matchesplayed.append(str(currentteam["matchesPlayed"]))
-      avgpoints.append(str(currentteam["qualAverage"]))
-  
-    rankingdf = pd.DataFrame({
-        'Rank': teamrank,
-        'Team Number': teamnumber,
-        'Wins/Ties/Losses': wtl,
-        'Average Points': avgpoints,
-        'Matches Played': matchesplayed,
-    })
-    def highlight_team(s):
-      color = '#262730' if str(s) == str(team) else ''
-      return f'background-color: {color}'
-    st.subheader("Event Rankings")
-    st.dataframe(rankingdf.style.applymap(highlight_team))
     
+    rankdata = getteamrank(team, year, currentevent, level)
+    scheduledf, teamrp = getschedule(team, year, currentevent, level)
+
+    displayteamdata(rankdata, teamrp)
+
+    displayschedule(team, eventstartdate, eventenddate, scheduledf)
+    
+    rankingdf = gettopteams(team, year, currentevent, level)
+    
+    displayrankings(team, rankingdf)
